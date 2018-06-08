3e6fe6e278342fd50f3eae52d7505058eacda9ac
Unknown <sisto94000@hotmail.fr>
2018-06-08 16:45:20 +0200

    Projet

---
+++ ProjetFinalSopra/src/main/java/org/sopra/SpringBootMvc/ProjetFinalSopraApplication.java
@@ -0,0 +1,12 @@
+package org.sopra.SpringBootMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalSopraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalSopraApplication.class, args);
+	}
+}
